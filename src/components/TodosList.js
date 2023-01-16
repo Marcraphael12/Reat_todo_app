4e6fdeeb385dd5d2f5ddaff1f2752b44386ee783
Marc raphael <marcraphael979@gmail.com>
2023-01-16 13:35:38 +0100

    components/TodosList: Render List

--- src/components/TodosList.js
+++ src/components/TodosList.js
@@ -1,13 +1,14 @@
-import React from "react";
+import React from "react"
 
-class TodosList extends React.Component  {
-	render() {
-		return (
-			<div>
-				Hello world
-			</div>
-		)
-	}
+class TodosList extends React.Component {
+  render() {
+    return (
+      <ul>
+        { this.props.todos.map(todo => (
+          <li>{todo.title}</li> ))}
+      </ul>
+    )
+  }
 }
 
-export default TodosList;
+export default TodosList
