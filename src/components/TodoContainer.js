0eb462431044dd85084546398acbefb3248189d1
Marc raphael <marcraphael979@gmail.com>
2023-01-17 15:31:09 +0100

    Fix eslint errors

--- src/components/TodoContainer.js
+++ src/components/TodoContainer.js
@@ -4,37 +4,37 @@ import Header from './Header';
 import TodosList from './TodosList';
 
 class TodoContainer extends React.Component {
-	state = {
-	  todos: [
-	    {
-	      id: 1,
-	      title: 'Setup development environment',
-	      completed: true,
-	    },
-	    {
-	      id: 2,
-	      title: 'Develop website and add content',
-	      completed: false,
-	    },
-	    {
-	      id: 3,
-	      title: 'Deploy to live server',
-	      completed: false,
-	    },
-	  ],
-	 };
+  state = {
+    todos: [
+      {
+        id: 1,
+        title: 'Setup development environment',
+        completed: true,
+      },
+      {
+        id: 2,
+        title: 'Develop website and add content',
+        completed: false,
+      },
+      {
+        id: 3,
+        title: 'Deploy to live server',
+        completed: false,
+      },
+    ],
+  };
 
-	 handleChange = (id) => {
-	   console.log('clicked', id);
-	 };
+    handleChange = (id) => {
+      console.log('clicked', id);
+    };
 
-	 render() {
-	   return (
-  <div>
-    <Header />
-    <TodosList todos={this.state.todos} handleChangeProps={this.handleChange} />
-  </div>
-	   );
-	 }
+    render() {
+      return (
+        <div>
+          <Header />
+          <TodosList todos={this.state.todos} handleChangeProps={this.handleChange} />
+        </div>
+      );
+    }
 }
 export default TodoContainer;
